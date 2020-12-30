cf11b22d9acc491fa8dd76a6aa4016403a68ea38
jiikko <n905i.1214@gmail.com>
2020-12-30 18:30:16 +0900

    first commit

---
+++ lib/rrails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rrails/version"
+
+module Rrails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rrails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rrails
+  VERSION = "0.1.0"
+end
